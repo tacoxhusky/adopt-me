354e6a6e0a675cdb755e6d9017e1d0e8eb7a8218
tacoxhusky <tacoxhusky@gmail.com>
2021-10-09 06:47:21 -0400

    add ESLint rules

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 
@@ -28,10 +27,10 @@ const App = () => {
 		<div>
 			<h1>Adopt Me!</h1>
 			<Pet name="Luna" animal="Dog" breed="Havanese" />
-			<Pet name="Testing" animal="Bird" breed="Cockatiel" />
+			<Pet name="Pepper" animal="Bird" breed="Cockatiel" />
 			<Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
 		</div>
 	);
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 // export const Pet = (props) => {
 // 	return React.createElement("div", {}, [
 // 		React.createElement("h2", {}, props.name),
